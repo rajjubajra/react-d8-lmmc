d8dd8a59c531b39e8153a719d611d086208767d3
rajjubajra <vajracharya@yahoo.com>
2019-04-05 14:49:23 +0100

    Updates

---
+++ precache-manifest.3a4e285329c9f58ca4095a19e519717c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "68556a9a1a0eba622f73",
+    "url": "/react-d8-lmmc/static/js/runtime~main.cd434208.js"
+  },
+  {
+    "revision": "1988873eb0c544ff8582",
+    "url": "/react-d8-lmmc/static/js/main.925e99f8.chunk.js"
+  },
+  {
+    "revision": "fa7176aba72e1889f1d9",
+    "url": "/react-d8-lmmc/static/js/2.d31592f1.chunk.js"
+  },
+  {
+    "revision": "1988873eb0c544ff8582",
+    "url": "/react-d8-lmmc/static/css/main.5c90d147.chunk.css"
+  },
+  {
+    "revision": "fa7176aba72e1889f1d9",
+    "url": "/react-d8-lmmc/static/css/2.c6203fe3.chunk.css"
+  },
+  {
+    "revision": "1fca7688f57483f606adc57806ab2f5b",
+    "url": "/react-d8-lmmc/index.html"
+  }
+];
